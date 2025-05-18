e88fda46dfbd9779f0086dff4ad278f952731674
Matthew Concepcion <matthewconcepcion10@gmail.com>
2025-05-19 02:34:09 +0800

    refactor: change backend service for solid principle

--- lol_backend_service.py
+++ lol_backend_service.py
@@ -1,17 +1,24 @@
 import requests
+from abc import ABC, abstractmethod
 from constants import Clusters
 
-def send_get_request(url):
-    response = requests.get(url)
-    if response.status_code != 200:
-        raise Exception(f"Status: {response.status_code}, Message: {response.text['status']['message']}")
-    data = response.json()
-    return data
+class APIService(ABC):
+    @abstractmethod
+    def send_get_request(cls, url: str):
+        raise NotImplementedError("Subclasses must implement this method")
 
-class RiotAPI:
+
+class RiotAPI(APIService):
     def __init__(self, api_key):
         self.api_key = api_key
 
+    @classmethod
+    def send_get_request(cls, url):
+        response = requests.get(url)
+        if response.status_code != 200:
+            raise requests.exceptions.HTTPError(f"Status: {response.status_code}, Message: {response.text['status']['message']}")
+        data = response.json()
+        return data
 
     def _url_builder(self, cluster, endpoint):
         url = endpoint
@@ -21,40 +28,45 @@ class RiotAPI:
             url = f"{Clusters.SEA.value}{url}"
         elif cluster == "SG2":
             url = f"{Clusters.SG2.value}{url}"
+        else:
+            raise ValueError("Invalid cluster specified.")
         url += f"&api_key={self.api_key}"
         return url
 
-    
+class RiotAPIFactory:
+    def __init__(self, api_key: RiotAPI):
+        self.riot_client = RiotAPI(api_key)
+
     def get_puuid_by_riot_id(self, game_name, tagline):
         endpoint = f"/riot/account/v1/accounts/by-riot-id/{game_name}/{tagline}?"
-        url = self._url_builder("ASIA", endpoint)
-        data = send_get_request(url)
+        url = self.riot_client._url_builder("ASIA", endpoint)
+        data = self.riot_client.send_get_request(url)
         return data['puuid']
     
 
     def get_riot_id_by_puuid(self, puuid):
         endpoint = f"/riot/account/v1/accounts/by-puuid/{puuid}?"
-        url = self._url_builder("ASIA", endpoint)
-        data = send_get_request(url)
+        url = self.riot_client._url_builder("ASIA", endpoint)
+        data = self.riot_client.send_get_request(url)
         return {'game_name': data['gameName'], 'tagline': data['tagLine']}
 
     
     def get_free_champ_rotation(self, cluster):
         endpoint = "/lol/platform/v3/champion-rotations?"
-        url = self._url_builder(cluster, endpoint)
-        data = send_get_request(url)
+        url = self.riot_client._url_builder(cluster, endpoint)
+        data = self.riot_client.send_get_request(url)
         return data['freeChampionIds']
     
 
     def get_match_list(self, cluster, puuid, start = 0, count = 20):
         endpoint = f"/lol/match/v5/matches/by-puuid/{puuid}/ids?start={start}&count={count}"
-        url = self._url_builder(cluster, endpoint)
-        data = send_get_request(url)
+        url = self.riot_client._url_builder(cluster, endpoint)
+        data = self.riot_client.send_get_request(url)
         return data
 
 
     def get_match_data(self, cluster, match_id):
         endpoint = f"/lol/match/v5/matches/{match_id}?"
-        url = self._url_builder(cluster, endpoint)
-        data = send_get_request(url)
+        url = self.riot_client._url_builder(cluster, endpoint)
+        data = self.riot_client.send_get_request(url)
         return data
